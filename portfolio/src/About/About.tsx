dfc3684dae18b759422a53f9d1dc98352dbd74fb
Caleb Elizondo <calebelizondo@tamu.edu>
2024-01-17 00:40:36 -0600

    updated about content

---
+++ portfolio/src/About/About.tsx
@@ -0,0 +1,31 @@
+const About = () => {
+    return (
+        <>
+            <div className="section-container light-brown">
+                <div>
+                    <h1 className="section-title">About me</h1>
+                    <div className="icon-container-small">
+                        <a href="tel:+12547224412"><i className="fas fa-phone small-icon"></i></a>
+                        <a href="mailto:calebelizondo@tamu.edu"><i className="far fa-envelope small-icon"></i></a>
+                        <a href="https://github.com/calebelizondo">
+                            <i className="fab fa-github small-icon"></i>   
+                        </a>
+                        <a href="https://www.linkedin.com/in/caleb-elizondo-08975a250/">
+                            <i className="fab fa-linkedin small-icon"></i>
+                        </a>
+                    </div>
+                </div>
+                <p>
+                    My name is Caleb Elizondo and I am a junior Computer Science major at Texas A&M. In addition
+                    to my coursework I also serve as the Managing Editor for the The Battalion, the 
+                    independent student newspaper. 
+                </p>
+                <p>
+
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default About;
